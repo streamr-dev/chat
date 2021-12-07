316567c934bd3e34d252495144d5762ceeb30ff0
mxinburritos <mxinburritos@gmail.com>
2021-12-07 05:48:30 -0500

    Add navbar component

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,13 +1,16 @@
-import Sidebar from "../src/components/Sidebar/Sidebar"
+import Navbar from '../src/components/Navbar/Navbar'
+import Sidebar from '../src/components/Sidebar/Sidebar'
 import styles from '../styles/Chat.module.scss'
 
 const Chat = (): JSX.Element => {
-  return <div className={styles.container}>
-    <h1>
-      Chat
-    </h1>
-    <Sidebar />
-  </div>
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.content}>
+        <Sidebar />
+      </div>
+    </div>
+  )
 }
 
 export default Chat
