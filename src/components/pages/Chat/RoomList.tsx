86e39a235cea11086ed096e063f84feb1c603955
Mariusz Roliński <mariusz@mondoreale.pl>
2022-01-03 23:51:34 +0100

    Buncha renaming
    - AddRoom → AddRoomItem
    - Sidebar → RoomList
    - Room → RoomItem

--- src/components/pages/Chat/RoomList.tsx
+++ src/components/pages/Chat/RoomList.tsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components'
-import AddRoom from './AddRoom'
+import AddRoom from './AddRoomItem'
 
 type Props = {
     className?: string,
     children?: React.ReactNode,
 }
 
-const UnstyledSidebar = ({ className, children }: Props) => (
+const UnstyledRoomList = ({ className, children }: Props) => (
     <aside className={className}>
         <AddRoom />
         {children}
     </aside>
 )
 
-const Sidebar = styled(UnstyledSidebar)`
+const RoomList = styled(UnstyledRoomList)`
     height: 100%;
     overflow: auto;
     width: 22rem;
 `
 
-export default Sidebar
+export default RoomList
