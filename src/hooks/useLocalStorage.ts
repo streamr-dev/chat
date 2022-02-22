c98a5bbbd66e9cdf925e3d62535ac68d3634dba7
bearni95 <bernatcanal@gmail.com>
2022-02-22 13:18:45 +0100

    updated getStorageValue method for simplicity

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 
 function getStorageValue(key: string, defaultValue: string) {
-    // getting stored value
-    if (typeof window !== 'undefined') {
-        const saved = localStorage.getItem(key)
-        const initial = saved !== null ? JSON.parse(saved) : defaultValue
-        return initial
+    try {
+        const item = localStorage.getItem(key)
+        if (!item) {
+            return defaultValue
+        }
+        return JSON.parse(item)
+    } catch (e) {
+        return defaultValue
     }
 }
 
