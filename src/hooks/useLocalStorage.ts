60591b58fbf99f4012d53d7f871680a74db2423c
bearni95 <bernatcanal@gmail.com>
2022-02-22 13:20:32 +0100

    added updateRef check on useLocalStorage useEffect callback

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function getStorageValue(key: string, defaultValue: string) {
     try {
@@ -13,14 +13,20 @@ function getStorageValue(key: string, defaultValue: string) {
 }
 
 export const useLocalStorage = (key: string, defaultValue = '') => {
+    const isUpdateRef = useRef<boolean>(false)
     const [value, setValue] = useState(() => {
         return getStorageValue(key, defaultValue)
     })
 
     useEffect(() => {
+        if (!isUpdateRef.current) {
+            isUpdateRef.current = true
+            // It's a on-mount run. Ignore the rest.
+            return
+        }
         // storing input name
         localStorage.setItem(key, JSON.stringify(value))
-    }, [key, value])
+    }, [key, value, isUpdateRef])
 
     return [value, setValue]
 }
