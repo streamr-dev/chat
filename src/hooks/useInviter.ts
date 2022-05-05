c551e848b37a8491b9da08dd610b12429fb399fd
bearni95 <bernatcanal@gmail.com>
2022-05-05 11:54:26 +0200

    merged master; fudged incoming with new code

--- src/hooks/useInviter.ts
+++ src/hooks/useInviter.ts
@@ -1,7 +1,5 @@
 import { useCallback } from 'react'
 import { Stream, StreamPermission } from 'streamr-client'
-import { useSend } from '../components/pages/Chat/MessageTransmitter'
-import { MessageType } from '../utils/types'
 import useEnsureMaticBalance from './useEnsureMaticBalance'
 
 type Options = {
@@ -14,18 +12,24 @@ type Inviter = ({ invitees, stream }: Options) => Promise<void>
 export default function useInviter(): Inviter {
     const ensureMaticBalance = useEnsureMaticBalance()
 
-    return useCallback(async ({ invitees, stream }: Options) => {
-        await ensureMaticBalance()
+    return useCallback(
+        async ({ invitees, stream }: Options) => {
+            await ensureMaticBalance()
 
-        const tasks: { user: string; permissions: StreamPermission[] }[] = []
-        for (let i = 0; i < invitees.length; i++) {
-            tasks.push({
-                user: invitees[i],
-                permissions: [StreamPermission.GRANT],
-            })
-        }
-        await stream.grantPermissions(...tasks)
+            const tasks: { user: string; permissions: StreamPermission[] }[] =
+                []
+            for (let i = 0; i < invitees.length; i++) {
+                tasks.push({
+                    user: invitees[i],
+                    permissions: [StreamPermission.GRANT],
+                })
+            }
+            await stream.grantPermissions(...tasks)
 
-        console.info(`Invited ${invitees.join(', ')} to stream ${stream.id}`)
-    }, [ensureMaticBalance])
+            console.info(
+                `Invited ${invitees.join(', ')} to stream ${stream.id}`
+            )
+        },
+        [ensureMaticBalance]
+    )
 }
--- src/hooks/useRevoker.ts
+++ src/hooks/useRevoker.ts
@@ -1,7 +1,5 @@
 import { useCallback } from 'react'
 import { Stream } from 'streamr-client'
-import { useSend } from '../components/pages/Chat/MessageTransmitter'
-import { MessageType } from '../utils/types'
 import useEnsureMaticBalance from './useEnsureMaticBalance'
 
 type Options = {
@@ -14,19 +12,22 @@ type Revoker = ({ revokee, stream }: Options) => Promise<void>
 export default function useRevoker(): Revoker {
     const ensureMaticBalance = useEnsureMaticBalance()
 
-    return useCallback(async ({ revokee, stream }: Options) => {
-        await ensureMaticBalance()
-        // fetch the specific permissions to revoke
-        const streamPermissions = await stream.getPermissions()
-        const [permissions] = streamPermissions
-            .filter((item) => {
-                return (item as any).user === revokee
-            })
-            .map((item) => item.permissions)
+    return useCallback(
+        async ({ revokee, stream }: Options) => {
+            await ensureMaticBalance()
+            // fetch the specific permissions to revoke
+            const streamPermissions = await stream.getPermissions()
+            const [permissions] = streamPermissions
+                .filter((item) => {
+                    return (item as any).user === revokee
+                })
+                .map((item) => item.permissions)
 
-        await stream.revokePermissions({
-            user: revokee,
-            permissions,
-        })
-    }, [ensureMaticBalance])
+            await stream.revokePermissions({
+                user: revokee,
+                permissions,
+            })
+        },
+        [ensureMaticBalance]
+    )
 }
