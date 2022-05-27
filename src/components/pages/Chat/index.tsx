4704cce4fdfe431e05861515344f6ac095ac9b90
Mariusz Roliński <mariusz@mondoreale.pl>
2022-05-27 08:19:17 +0200

    Tweaks

--- src/components/pages/Chat/index.tsx
+++ src/components/pages/Chat/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import tw from 'twin.macro'
 import Page from '../../Page'
