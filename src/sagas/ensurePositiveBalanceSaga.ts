b3db5fe3a27f3dd12b92060b424f97f4bd1ab9c3
Mariusz Roliński <mariusz@mondoreale.pl>
2022-05-29 21:28:01 +0200

    Impl. balance check saga

--- src/sagas/ensurePositiveBalanceSaga.ts
+++ src/sagas/ensurePositiveBalanceSaga.ts
@@ -1,4 +1,28 @@
-// eslint-disable-next-line require-yield
+import { Provider } from '@web3-react/types'
+import { BigNumber, providers } from 'ethers'
+import { select } from 'redux-saga/effects'
+import { OptionalAddress } from '../../types/common'
+import InsufficientFundsError from '../errors/InsufficientFundsError'
+import MissingWalletAccountError from '../errors/MissingWalletAccountError'
+import MissingWalletProviderError from '../errors/MissingWalletProviderError'
+import { selectWalletAccount, selectWalletProvider } from '../features/wallet/selectors'
+
 export default function* ensurePositiveBalanceSaga() {
-    console.warn('ensurePositiveBalanceSaga: not implemented.')
+    const account: OptionalAddress = yield select(selectWalletAccount)
+
+    if (!account) {
+        throw new MissingWalletAccountError()
+    }
+
+    const provider: undefined | Provider = yield select(selectWalletProvider)
+
+    if (!provider) {
+        throw new MissingWalletProviderError()
+    }
+
+    const balance: BigNumber = yield new providers.Web3Provider(provider).getBalance(account)
+
+    if (balance.eq(0)) {
+        throw new InsufficientFundsError()
+    }
 }
