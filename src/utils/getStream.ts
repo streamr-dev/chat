877b4207529398d77b10307fdeee73bff787b973
Mariusz Roliński <mariusz@mondoreale.pl>
2022-06-14 13:12:28 +0200

    FRONT-1007: Make `getStream` suppress errors

--- src/utils/getStream.ts
+++ src/utils/getStream.ts
@@ -1,12 +1,19 @@
 import StreamrClient from 'streamr-client'
 import { EnhancedStream } from '$/types'
 import sanitizeStream from './sanitizeStream'
+import handleError from '$/utils/handleError'
 
 export default async function getStream(
     client: StreamrClient,
     id: string
 ): Promise<undefined | EnhancedStream> {
-    const stream = await client.getStream(id)
+    let stream
+
+    try {
+        stream = await client.getStream(id)
+    } catch (e) {
+        handleError(e)
+    }
 
     return stream ? sanitizeStream(stream) : undefined
 }
