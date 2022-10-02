41eb0277450b0355f5aa6da237bd3a045eaa1f0f
Mariusz Roliński <mariusz@mondoreale.pl>
2022-10-02 19:50:10 +0200

    Show loading indicator while resending

---
+++ src/hooks/useIsResending.ts
@@ -0,0 +1,33 @@
+import { RoomId } from '$/features/room/types'
+import { OptionalAddress, State } from '$/types'
+import isSameAddress from '$/utils/isSameAddress'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+
+function selectFlags(state: State) {
+    return state.flag
+}
+
+export default function useIsResending(roomId: undefined | RoomId, requester: OptionalAddress) {
+    const flags = useSelector(selectFlags)
+
+    return useMemo(() => {
+        if (!roomId || !requester) {
+            return false
+        }
+
+        return Object.keys(flags).some((flag) => {
+            const [key, rid, addr] = JSON.parse(flag)
+
+            if (key !== 'isResendingMessage' && key !== 'isResendingMessagesForSpecificDay') {
+                return false
+            }
+
+            if (roomId !== rid || !isSameAddress(addr, requester)) {
+                return false
+            }
+
+            return true
+        })
+    }, [flags, roomId, requester])
+}
