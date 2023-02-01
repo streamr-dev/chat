6d5909f51074a4b5b62ec1b455c8a1ce282e6347
Mariusz Roliński <mariusz@mondoreale.pl>
2023-02-01 15:02:42 +0100

    FRONT-1189: Make modals nicely scrollable

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,7 +1,8 @@
 import gsap from 'gsap'
 import { HTMLAttributes, ReactNode, useEffect, useRef } from 'react'
 import useGlobalKeyDownEffect from 'streamr-ui/hooks/useGlobalKeyDownEffect'
-import tw from 'twin.macro'
+import useOnMouseDownOutsideEffect from 'streamr-ui/hooks/useOnMouseDownOutsideEffect'
+import tw, { css } from 'twin.macro'
 
 export enum AbortReason {
     CloseButton,
@@ -79,126 +80,149 @@ export default function Modal({
         }
     })
 
+    const modalRef = useOnMouseDownOutsideEffect(() => void close(AbortReason.Backdrop))
+
     return (
-        <>
-            {/* Backdrop. */}
+        <div
+            css={[
+                tw`
+                    bg-[rgba(0, 0, 0, 0.3)]
+                    backdrop-blur
+                    fixed
+                    w-full
+                    h-full
+                    top-0
+                    left-0
+                    overflow-auto
+                `,
+            ]}
+        >
             <div
-                onMouseDown={() => void close(AbortReason.Backdrop)}
                 css={[
                     tw`
-                        bg-[rgba(0, 0, 0, 0.3)]
-                        backdrop-blur
-                        fixed
-                        top-0
-                        left-0
-                        w-full
+                        flex
+                        items-center
+                        justify-center
                         h-full
-                    `,
-                ]}
-            />
-            {/* Modal. */}
-            <div
-                css={[
-                    tw`
-                        -translate-x-1/2
-                        -translate-y-1/2
-                        fixed
-                        left-1/2
-                        max-w-[560px]
-                        top-1/2
-                        w-[90vw]
+                        relative
                     `,
                 ]}
             >
                 <div
                     css={[
                         tw`
-                        animate-[bringIn 150ms ease-in-out 1]
+                            max-h-full
+                            overflow-visible
                         `,
                     ]}
                 >
                     <div
-                        ref={wigglyRef}
                         css={[
                             tw`
-                                bg-[white]
-                                p-12
-                                pt-8
-                                rounded-[20px]
-                                shadow-lg
+                                py-16
                             `,
                         ]}
                     >
                         <div
+                            ref={modalRef}
                             css={[
                                 tw`
-                                    flex
-                                    items-center
-                                    mb-6
+                                    pointer-events-auto
+                                    max-w-[560px]
+                                    w-[90vw]
                                 `,
                             ]}
                         >
-                            <div tw="flex-grow min-w-0">
-                                <h2
+                            <div
+                                css={[
+                                    tw`
+                                        animate-[bringIn 150ms ease-in-out 1]
+                                    `,
+                                ]}
+                            >
+                                <div
+                                    ref={wigglyRef}
                                     css={[
                                         tw`
-                                            font-medium
-                                            text-[1.25rem]
-                                            truncate
+                                            bg-[white]
+                                            p-12
+                                            pt-8
+                                            rounded-[20px]
+                                            shadow-lg
                                         `,
                                     ]}
                                 >
-                                    {title}
-                                </h2>
-                                {!!subtitle && (
-                                    <p
+                                    <div
                                         css={[
                                             tw`
-                                                text-[#59799C]
-                                                text-[0.875rem]
-                                                truncate
+                                                flex
+                                                items-center
+                                                mb-6
                                             `,
                                         ]}
                                     >
-                                        {subtitle}
-                                    </p>
-                                )}
-                            </div>
-                            <div>
-                                <button
-                                    type="button"
-                                    css={[
-                                        tw`
-                                            block
-                                            appearance-none
-                                            [svg]:block
-                                        `,
-                                    ]}
-                                    onClick={() => void close(AbortReason.CloseButton)}
-                                >
-                                    <svg
-                                        width="32"
-                                        height="32"
-                                        viewBox="0 0 32 32"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            clipRule="evenodd"
-                                            d="M8.47 8.47a.75.75 0 0 1 1.06 0L16 14.94l6.47-6.47a.75.75 0 1 1 1.06 1.06L17.06 16l6.47 6.47a.75.75 0 1 1-1.06 1.06L16 17.06l-6.47 6.47a.75.75 0 0 1-1.06-1.06L14.94 16 8.47 9.53a.75.75 0 0 1 0-1.06z"
-                                            fill="#59799C"
-                                        />
-                                    </svg>
-                                </button>
+                                        <div tw="grow min-w-0">
+                                            <h2
+                                                css={[
+                                                    tw`
+                                                        font-medium
+                                                        text-[1.25rem]
+                                                        truncate
+                                                    `,
+                                                ]}
+                                            >
+                                                {title}
+                                            </h2>
+                                            {!!subtitle && (
+                                                <p
+                                                    css={[
+                                                        tw`
+                                                            text-[#59799C]
+                                                            text-[0.875rem]
+                                                            truncate
+                                                        `,
+                                                    ]}
+                                                >
+                                                    {subtitle}
+                                                </p>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <button
+                                                type="button"
+                                                css={[
+                                                    tw`
+                                                        block
+                                                        appearance-none
+                                                        [svg]:block
+                                                    `,
+                                                ]}
+                                                onClick={() => void close(AbortReason.CloseButton)}
+                                            >
+                                                <svg
+                                                    width="32"
+                                                    height="32"
+                                                    viewBox="0 0 32 32"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        clipRule="evenodd"
+                                                        d="M8.47 8.47a.75.75 0 0 1 1.06 0L16 14.94l6.47-6.47a.75.75 0 1 1 1.06 1.06L17.06 16l6.47 6.47a.75.75 0 1 1-1.06 1.06L16 17.06l-6.47 6.47a.75.75 0 0 1-1.06-1.06L14.94 16 8.47 9.53a.75.75 0 0 1 0-1.06z"
+                                                        fill="#59799C"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    {children}
+                                </div>
                             </div>
                         </div>
-                        {children}
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
-
-Modal.displayName = 'Modal'
