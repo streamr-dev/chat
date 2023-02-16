1d07a1edea4c67008f3c1b00c0b6f905d2bf9ddd
Mariusz Roliński <mariusz@mondoreale.pl>
2023-02-16 15:47:07 +0100

    Fix `retoast` (use consistent return types)

--- src/features/toaster/helpers/retoast.ts
+++ src/features/toaster/helpers/retoast.ts
@@ -4,13 +4,13 @@ import { ComponentProps } from 'react'
 import { call } from 'redux-saga/effects'
 
 export default function retoast(tc: Controller | undefined, props: ComponentProps<typeof Toast>) {
-    return call(function () {
-        if (tc) {
+    if (tc) {
+        return call(function () {
             tc.update(props)
 
             return tc
-        }
+        })
+    }
 
-        return toast(props)
-    })
+    return toast(props)
 }
