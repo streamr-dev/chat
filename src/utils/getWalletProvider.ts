9e29780516b198d0f2ef7ae4de426432849c3824
Mariusz Roliński <mariusz@mondoreale.pl>
2023-03-09 14:07:35 +0100

    Use provider directly from the connector

--- src/utils/getWalletProvider.ts
+++ src/utils/getWalletProvider.ts
@@ -1,10 +1,20 @@
 import MissingWalletProviderError from '$/errors/MissingWalletProviderError'
-import { selectWalletProvider } from '$/features/wallet/selectors'
+import { selectWalletIntegrationId } from '$/features/wallet/selectors'
+import { WalletIntegrationId } from '$/features/wallet/types'
+import getConnector from '$/utils/getConnector'
 import { Provider } from '@web3-react/types'
-import { select } from 'redux-saga/effects'
+import { call, select } from 'redux-saga/effects'
 
 export default function* getWalletProvider() {
-    const provider: Provider | undefined = yield select(selectWalletProvider)
+    let provider: Provider | undefined
+
+    yield call(function* () {
+        const integrationId: WalletIntegrationId | undefined = yield select(
+            selectWalletIntegrationId
+        )
+
+        provider = getConnector(integrationId)[0].provider
+    })
 
     if (!provider) {
         throw new MissingWalletProviderError()
