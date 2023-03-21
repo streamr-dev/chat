8311a4fe37f3b1d5e04592f687e7936943d83e2a
Mariusz Roliński <mariusz@mondoreale.pl>
2023-03-21 23:45:35 +0100

    Notify about insufficient funds

--- src/utils/preflight.ts
+++ src/utils/preflight.ts
@@ -2,19 +2,35 @@ import { BigNumber, providers } from 'ethers'
 import { Address } from '$/types'
 import networkPreflight from './networkPreflight'
 import InsufficientFundsError from '$/errors/InsufficientFundsError'
-import { call } from 'redux-saga/effects'
+import { call, put } from 'redux-saga/effects'
 import { JSON_RPC_URL } from '$/consts'
+import { ToasterAction } from '$/features/toaster'
+import { ToastType } from '$/components/Toast'
 
 export default function preflight(account: Address) {
     return call(function* () {
-        yield networkPreflight()
+        try {
+            yield networkPreflight()
 
-        const balance: BigNumber = yield new providers.JsonRpcProvider(JSON_RPC_URL).getBalance(
-            account
-        )
+            const balance: BigNumber = yield new providers.JsonRpcProvider(JSON_RPC_URL).getBalance(
+                account
+            )
 
-        if (balance.eq(0)) {
-            throw new InsufficientFundsError()
+            if (balance.eq(0)) {
+                throw new InsufficientFundsError()
+            }
+        } catch (e) {
+            if (e instanceof InsufficientFundsError) {
+                yield put(
+                    ToasterAction.show({
+                        title: 'Insufficient funds',
+                        desc: "You don't have enough MATIC.",
+                        type: ToastType.Error,
+                    })
+                )
+            }
+
+            throw e
         }
     })
 }
